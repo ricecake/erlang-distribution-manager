3142504f6057eb675e5f8ff9fcf3c697a646798f
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-04-07 18:46:50 -0400

    change some api funciton names around.
    Were a little to close to the interface function names

--- src/dman_worker.erl
+++ src/dman_worker.erl
@@ -26,18 +26,21 @@ start_link() ->
 
 behaviour_info(callbacks) -> 
 	[
-		{add_task, 2},
-		{remove_task, 2},
-		{list_tasks, 2},
+		{handle_add, 2},
+		{handle_remove, 2},
+		{handle_list, 2},
 
-		{get_status, 2},
+		{handle_status, 2},
 
-		{add_cohort, 2},
-		{remove_cohort,2},
+		{handle_add_cohort, 2},
+		{handle_remove_cohort,2},
 
 		{init, 2},
 		{terminate,2},
-		{code_change,3}
+		{code_change,3},
+		{handle_call,3},
+		{handle_cast,2},
+		{handle_info,2}
 	];
 behaviour_info(_Other) -> undefined.
 %% ------------------------------------------------------------------
