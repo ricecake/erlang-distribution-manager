2fa557c35169127a5c66beaf36307b711fa6cd29
Sebastian Green-Husted <ricecake@tfm.nu>
2014-05-04 23:18:49 -0400

    stub out job addition functions

--- src/dman_router.erl
+++ src/dman_router.erl
@@ -84,6 +84,16 @@ handle_cast({attach, SNode}, State) ->
 	net_adm:ping(SNode),
 	{noreply, State};
 
+handle_cast({add, {Key, _JobDetails} = Job}, #state{buckets=Buckets} = State) ->
+	Bucket = getBucketForKey(Key),
+	{_epoch, Nodes}  = proplists:get_value(Bucket, Buckets),
+	[gen_gossip:cast({dman_router, Node}, {do_add, Job}) || Node <- Nodes],
+	{noreply, State};
+
+handle_cast({do_add, {Key, {SubSystem, Details}}}, State) ->
+	io:format("Key: ~p~nSub: ~p~nDetails: ~p~n",[Key, SubSystem, Details]),
+	{noreply, State};
+	
 handle_cast(_Message, State) ->
 	{noreply, State}.
 
