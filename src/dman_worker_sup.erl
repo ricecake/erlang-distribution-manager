7d9d5dda8ab7169d3775f22ced3918c48ab150b3
Sebastian Green-Husted <ricecake@tfm.nu>
2014-05-14 22:24:24 -0400

    minor changes for ease of use

--- src/dman_worker_sup.erl
+++ src/dman_worker_sup.erl
@@ -3,7 +3,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0, create/1]).
+-export([start_link/0, create/1, create/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -18,7 +18,8 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-create(Args) -> supervisor:start_child(?MODULE, [Args]).
+create(Module) -> create(Module, []).
+create(Module, Args) -> supervisor:start_child(?MODULE, [Module, Args, []]).
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
